478f5fb72e3190e097ab6792bdb8b994de5f1c38
Makhosandile <56253310+makhosi6@users.noreply.github.com>
2024-08-10 22:56:37 +0200

    Sat Aug 10 22:56:37 SAST 2024 - fix more broken links, add form validation, add conformation for form actions

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,4 +1,7 @@
-const formHTML = document.querySelector('#waitlist-form') || document.querySelector('form');;
+const formHTML = document.querySelector('#waitlist-form') || document.querySelector('form');
+const snackbarHTML = document.getElementById("snackbar");
+const SNACKBAR_TIMEOUT = 3500;
+
 formHTML.addEventListener('submit', function (event) {
     event.preventDefault();
 
@@ -6,6 +9,10 @@ formHTML.addEventListener('submit', function (event) {
 
     if (form.checkValidity()) {
         const formData = new FormData(form);
+        const email = formData.get('email');
+        const urlParams = new URLSearchParams(window.location.search);
+        const name = urlParams.get('platform') || 'unknown';
+        const message = "Under construction notification";
 
         fetch("https://byteestudio.com/api/send-contact-info", {
             "headers": {
@@ -13,18 +20,49 @@ formHTML.addEventListener('submit', function (event) {
                 "content-type": "application/json",
             },
             "referrer": "https://www.flexiipay.com",
-            "body": formData,
+            "body": JSON.stringify({ email, name, message }),
             "method": "POST",
             "mode": "no-cors",
         })
-            .then(response => response.text())
+            .then(response => {
+                
+                if (response.status > 399 && response.status != 429) {
+                    throw new Error( response.statusText +  ' Try again later.');
+                }
+
+                return response.text()
+            })
             .then(data => {
+                snackbarHTML.innerText = "Success! Thanks for submitting";
+                snackbarHTML.classList.add('show')
+                snackbarHTML.classList.add('btn-warning')
+
+                setTimeout(function () {
+                    snackbarHTML.classList.remove('btn-warning')
+                    snackbarHTML.classList.remove('show')
+                }, SNACKBAR_TIMEOUT);
+                formHTML.reset();
                 console.log('Success:', data);
             })
             .catch(error => {
+                snackbarHTML.innerText = 'Error: ' + error.toString();
+                snackbarHTML.classList.add('show')
+                snackbarHTML.classList.add('btn-secondary')
+
+                setTimeout(function () {
+                    snackbarHTML.classList.remove('btn-secondary')
+                    snackbarHTML.classList.remove('show')
+                }, SNACKBAR_TIMEOUT);
                 console.error('Error:', error);
             });
     } else {
-        alert('Please fill out all required fields.');
+        snackbarHTML.innerText = "Please fill out all required fields."
+        snackbarHTML.classList.add('show')
+        snackbarHTML.classList.add('btn-secondary')
+
+        setTimeout(function () {
+            snackbarHTML.classList.remove('btn-secondary')
+            snackbarHTML.classList.remove('show')
+        }, SNACKBAR_TIMEOUT);
     }
 });
